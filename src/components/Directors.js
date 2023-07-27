d9209dac7fa56aef2900ddffcc652e8c9b2ebe72
Ian Raburu <williamraburu@gmail.com>
2023-07-27 21:38:26 +0300

    Completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { directors } from "../data";
+import { directors, movies } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/
+    <>
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <div key={index}>{director.name}
+          <ul>
+            {director.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>}</div>;
 }
 
 export default Directors;
